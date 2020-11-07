52232dc920131c2efebe56acf48df93d092a4367
slowturtle99 <slowturtle99@naver.com>
2020-11-08 05:08:30 +0900

    kcy

---
+++ KCY/RLearn/2-cartpole/4-reinforce/test.py
@@ -0,0 +1,74 @@
+import sys
+import gym
+import copy
+import pylab
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+
+# 상태가 입력, 각 행동의 확률이 출력인 인공신경망 생성
+class REINFORCE(tf.keras.Model):
+    def __init__(self, action_size):
+        super(REINFORCE, self).__init__()
+        self.fc1 = Dense(24, activation='relu')
+        self.fc2 = Dense(24, activation='relu')
+        self.fc_out = Dense(action_size, activation='softmax')
+
+    def call(self, x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        policy = self.fc_out(x)
+        return policy
+
+
+# 그리드월드 예제에서의 REINFORCE 에이전트
+class REINFORCEAgent:
+    def __init__(self, state_size, action_size):
+        # 상태의 크기와 행동의 크기 정의
+        self.state_size = state_size
+        self.action_size = action_size
+
+        self.model = REINFORCE(self.action_size)
+        self.model.load_weights('save_model/trained/model')
+
+    # 정책신경망으로 행동 선택
+    def get_action(self, state):
+        policy = self.model(state)[0]
+        return np.argmax(policy)
+
+
+if __name__ == "__main__":
+    # 환경과 에이전트 생성
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    
+    action_size = env.action_space.n
+    agent = REINFORCEAgent(state_size, action_size)
+
+    EPISODES = 10
+    for e in range(EPISODES):
+        done = False
+        score = 0
+        # env 초기화
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+
+        while not done:
+            env.render()
+
+            # 현재 상태에 대한 행동 선택
+            action = agent.get_action(state)
+
+            # 선택한 행동으로 환경에서 한 타임스텝 진행 후 샘플 수집
+            next_state, reward, done, info = env.step(action)
+            next_state = np.reshape(next_state, [1, state_size])
+
+            score += reward
+
+            state = next_state
+
+            if done:
+                print("episode: {:3d} | score: {:3f}".format(e, score))
---
+++ KCY/RLearn/2-cartpole/4-reinforce/train.py
@@ -0,0 +1,130 @@
+import os
+import sys
+import gym
+import copy
+import pylab
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+class REINFORCE(tf.keras.Model):
+    def __init__(self, action_size):
+        super(REINFORCE, self).__init__()
+        self.fc1 = Dense(24, activation='relu')
+        self.fc2 = Dense(24, activation='relu')
+        self.fc_out = Dense(action_size, activation='softmax')
+    
+    def call(self,x):
+        x = self.fc1(x)
+        x = self.fc2(x)
+        policy = self.fc_out(x)
+        return policy
+
+class REINFORCEAgent:
+    def __init__(self, state_size, action_size):
+        self.render = False
+        self.state_size = state_size
+        self.action_size = action_size
+
+        self.discount_factor = 0.99
+        self.learning_rate = 0.001
+
+        self.model = REINFORCE(self.action_size)
+        self.optimizer = Adam(lr=self.learning_rate)
+
+        self.states, self.actions, self.rewards = [], [], []
+
+    def get_action(self, state):
+        policy = self.model(state)[0]
+        policy = np.array(policy)
+        return np.random.choice(self.action_size, 1, p=policy)[0]
+    
+    def discount_rewards(self, rewards):
+        discount_rewards = np.zeros_like(rewards)
+        running_add = 0
+        for t in reversed(range(0, len(rewards))):
+            running_add = running_add * self.discount_factor + rewards[t]
+            discount_rewards[t] = running_add
+        return discount_rewards
+
+    
+    def append_sample(self, state, action, reward):
+        self.states.append(state[0])
+        self.rewards.append(reward)
+        act = np.zeros(self.action_size)
+        act[action] = 1
+        self.actions.append(act)
+
+    def train_model(self):
+        discount_rewards = np.float32(self.discount_rewards(self.rewards))
+        discount_rewards -= np.mean(discount_rewards)
+        discount_rewards /= np.std(discount_rewards)
+
+        model_params = self.model.trainable_variables
+        with tf.GradientTape() as tape:
+            tape.watch(model_params)
+            policies = self.model(np.array(self.states))
+            actions = np.array(self.actions)
+            action_prob = tf.reduce_sum(actions * policies, axis=1)
+            cross_entropy = -tf.math.log(action_prob + 1e-5)
+            loss = tf.reduce_sum(cross_entropy * discount_rewards)
+            entropy = - policies * tf.math.log(policies)
+        
+        grads = tape.gradient(loss, model_params)
+        self.optimizer.apply_gradients(zip(grads, model_params))
+        self.states, self.actions, self.rewards = [], [], []
+
+        return np.mean(entropy)
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    
+    action_size = env.action_space.n
+
+    agent = REINFORCEAgent(state_size, action_size)
+
+    scores, episodes = [], []
+
+    EPISODES = 201
+
+    for e in range(EPISODES):
+        done = False
+        score = 0
+
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+
+        while not done:
+            if agent.render:
+                env.render()
+            
+            action = agent.get_action(state)
+
+            next_state, reward, done, info = env.step(action)
+            next_state = np.reshape(next_state, [1, state_size])
+
+            score += reward
+            reward = 0.1 if not done or score == 500 else -1
+
+            agent.append_sample(state,action,reward)
+            
+
+            state = next_state
+
+            if done:
+                entropy = agent.train_model()
+                print("episode: {:3d} | score: {:3f} | entropy: {:.3f}".format(
+                      e, score, entropy))
+
+                scores.append(score)
+                episodes.append(e)
+                pylab.plot(episodes, scores, 'b')
+                pylab.xlabel("episode")
+                pylab.ylabel("score")
+                pylab.savefig("./save_graph/graph.png")
+    
+    if e % 100 == 0:
+        agent.model.save_weights('save_model/model', save_format='tf')
